91873b8d1682aef31c05ef47d6b18182c9142b31
camila.assis <cabukater@gmail.com>
2021-03-22 18:37:10 -0300

    inserido footer

--- projetos/rsu/src/components/theme/Footer/index.js
+++ projetos/rsu/src/components/theme/Footer/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
 
 export default function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <AppBar position="static" color="primary">
+          <Container maxWidth="md">
+            <Toolbar>
+              <Typography variant="body1" color="inherit">
+                © Camila - 2021
+              </Typography>
+            </Toolbar>
+          </Container>
+        </AppBar>
     )
-}
+};
--- projetos/rsu/src/pages/Home/index.js
+++ projetos/rsu/src/pages/Home/index.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
 import CardBox from 'components/CardBox'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
- class HomePage extends Component {
-    render() {
-        return (
-            <div>
-               <CardBox/>
-            </div>
-        );
-    }
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-export default HomePage;
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
+  };
+
+  return (
+    <Grid container className={classes.root} spacing={2}>
+       <Grid item xs={12}>
+        <Paper className={classes.control}>
+          <Grid container justify="center" spacing={spacing}>
+            <Grid item spacing={2}>
+              <CardBox/>
+            </Grid>
+
+            <Grid item spacing={2}>
+              <CardBox/>
+            </Grid>
+
+            <Grid item spacing={2}>
+              <CardBox/>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
