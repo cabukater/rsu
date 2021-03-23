0a466f3ea0958a55f629f9835a60e9106ff44646
camila.assis <cabukater@gmail.com>
2021-03-23 12:43:53 -0300

    insert menu inside template

--- projetos/rsu/src/index.js
+++ projetos/rsu/src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Routing from "./routes.js";
-import Header from 'components/theme/Header';
-import Footer from  'components/theme/Footer'
-import './App.css';
+import Header from "theme/Header";
+import MenuBar from "theme/Menu";
+import Footer from "theme/Footer";
+import "assets/scss/Main.scss"
 
 ReactDOM.render(
   <>
-  <Header></Header>
-  <BrowserRouter>
-			<Routing	/>
-  </BrowserRouter>
-  <Footer></Footer>
-</>,
-  document.getElementById('root')
+    <flexItem>  <Header></Header></flexItem>
+    <flexItem>
+     
+      <BrowserRouter>
+      <MenuBar/>
+        <Routing />
+      </BrowserRouter>
+    </flexItem>
+    <flexItem>
+      <Footer></Footer>
+    </flexItem>
+  </>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
